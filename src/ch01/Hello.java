bb30c5c37cdbaa78b89efdcad9a7f474a47bec05
Cyeon <yeoneeso@naver.com>
2023-09-22 10:15:28 +0900

    두개의 파일이 수정되었습니다.

--- src/ch01/Hello.java
+++ src/ch01/Hello.java
@@ -4,5 +4,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("bonjour 01");
         System.out.println("bonjour 02");
+        System.out.println("bonjour 03");
     }
 }
--- src/ch01/World.java
+++ src/ch01/World.java
@@ -3,5 +3,6 @@ package ch01;
 public class World {
     public static void main(String[] args) {
         System.out.println("le monde 01");
+        System.out.println("le monde 02");
     }
 }
